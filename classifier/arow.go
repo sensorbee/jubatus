5a308d01009014e387de1d6f1dc6d69ce02b6c47
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-07-21 13:05:11 +0900

    Replace float64 with float32.

--- classifier/arow.go
+++ classifier/arow.go
@@ -7,10 +7,10 @@ import (
 
 type Arow struct {
 	storage
-	regWeight float64
+	regWeight float32
 }
 
-func NewArow(regWeight float64) (*Arow, error) {
+func NewArow(regWeight float32) (*Arow, error) {
 	if regWeight <= 0 {
 		return nil, errors.New("regularization weight must be larger than zero.")
 	}
@@ -39,8 +39,8 @@ func (a *Arow) Train(v FeatureVector, label Label) error {
 
 	variance := calcVariance(v, a.storage[label], a.storage[incorr.labelOrElse("")])
 
-	var beta float64 = 1 / (variance + 1/a.regWeight)
-	var alpha float64 = (1 + margin) * beta
+	var beta float32 = 1 / (variance + 1/a.regWeight)
+	var alpha float32 = (1 + margin) * beta
 
 	var incorrWeights weights
 	if incorr != nil {
@@ -72,21 +72,21 @@ func (a *Arow) Clear() {
 	a.storage = make(storage)
 }
 
-func (a *Arow) RegWeight() float64 {
+func (a *Arow) RegWeight() float32 {
 	return a.regWeight
 }
 
 type Dim string
 type FeatureElement struct {
 	Dim
-	Value float64
+	Value float32
 }
 type FeatureVector []FeatureElement
 
 type Label string
 type weight struct {
-	weight     float64
-	confidence float64
+	weight     float32
+	confidence float32
 }
 type weights map[Dim]weight
 type storage map[Label]weights
@@ -98,15 +98,15 @@ func initialWeight() weight {
 	}
 }
 
-func (ws weights) negativeUpdate(alpha, beta float64, dim Dim, x float64) {
+func (ws weights) negativeUpdate(alpha, beta float32, dim Dim, x float32) {
 	ws.update(alpha, beta, dim, x, (*weight).negativeUpdate)
 }
 
-func (ws weights) positiveUpdate(alpha, beta float64, dim Dim, x float64) {
+func (ws weights) positiveUpdate(alpha, beta float32, dim Dim, x float32) {
 	ws.update(alpha, beta, dim, x, (*weight).positiveUpdate)
 }
 
-func (ws weights) update(alpha, beta float64, dim Dim, x float64, f weightUpdateFunction) {
+func (ws weights) update(alpha, beta float32, dim Dim, x float32, f weightUpdateFunction) {
 	var weight weight
 	if w, ok := ws[dim]; ok {
 		weight = w
@@ -117,9 +117,9 @@ func (ws weights) update(alpha, beta float64, dim Dim, x float64, f weightUpdate
 	ws[dim] = weight
 }
 
-type weightUpdateFunction func(w *weight, alpha, beta, x float64)
+type weightUpdateFunction func(w *weight, alpha, beta, x float32)
 
-func (w *weight) negativeUpdate(alpha, beta, x float64) {
+func (w *weight) negativeUpdate(alpha, beta, x float32) {
 	aTerm := w.calcAlphaTerm(alpha, x)
 	bTerm := w.calcBetaTerm(beta, x)
 	w.weight -= aTerm
@@ -127,7 +127,7 @@ func (w *weight) negativeUpdate(alpha, beta, x float64) {
 	return
 }
 
-func (w *weight) positiveUpdate(alpha, beta, x float64) {
+func (w *weight) positiveUpdate(alpha, beta, x float32) {
 	aTerm := w.calcAlphaTerm(alpha, x)
 	bTerm := w.calcBetaTerm(beta, x)
 	w.weight += aTerm
@@ -135,18 +135,18 @@ func (w *weight) positiveUpdate(alpha, beta, x float64) {
 	return
 }
 
-func (w *weight) calcAlphaTerm(alpha, x float64) float64 {
+func (w *weight) calcAlphaTerm(alpha, x float32) float32 {
 	return alpha * w.confidence * x
 }
 
-func (w *weight) calcBetaTerm(beta, x float64) float64 {
+func (w *weight) calcBetaTerm(beta, x float32) float32 {
 	conf := w.confidence
 	return beta * conf * conf * x * x
 }
 
 type LScore struct {
 	Label Label
-	Score float64
+	Score float32
 }
 
 func (ls *LScore) labelOrElse(defaultL Label) Label {
@@ -156,7 +156,7 @@ func (ls *LScore) labelOrElse(defaultL Label) Label {
 	return ls.Label
 }
 
-func (ls *LScore) scoreOrElse(defaultS float64) float64 {
+func (ls *LScore) scoreOrElse(defaultS float32) float32 {
 	if ls == nil {
 		return defaultS
 	}
@@ -245,12 +245,12 @@ func (s storage) calcScores(v FeatureVector) LScores {
 	return scores
 }
 
-func calcMargin(correct *LScore, incorrect *LScore) float64 {
+func calcMargin(correct *LScore, incorrect *LScore) float32 {
 	return incorrect.scoreOrElse(0) - correct.scoreOrElse(0)
 }
 
-func calcVariance(v FeatureVector, w1, w2 weights) float64 {
-	variance := 0.0
+func calcVariance(v FeatureVector, w1, w2 weights) float32 {
+	var variance float32 = 0
 	for _, elem := range v {
 		dim := elem.Dim
 		val := elem.Value
@@ -260,7 +260,7 @@ func calcVariance(v FeatureVector, w1, w2 weights) float64 {
 }
 
 // TODO: consider to rename
-func (ws weights) covar(dim Dim) float64 {
+func (ws weights) covar(dim Dim) float32 {
 	if ws == nil {
 		return 1
 	}
