98b3800f16484712b42a97914e260aa10543e779
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-07-28 16:06:23 +0900

    Write godoc for common/intern.

--- common/intern/intern.go
+++ common/intern/intern.go
@@ -1,10 +1,12 @@
 package intern
 
+// Intern is a mapping from strings to ints.
 type Intern struct {
 	storage map[string]int
 	gen     int
 }
 
+// New creates a new Intern instance.
 func New() *Intern {
 	return &Intern{
 		storage: make(map[string]int),
@@ -12,10 +14,14 @@ func New() *Intern {
 	}
 }
 
+// GetOrZero returns an ID for a string if the string is already registered.
+// If the string is not registered this method returns zero,
 func (i *Intern) GetOrZero(s string) int {
 	return i.storage[s]
 }
 
+// Get returns an ID for a string. If the string was not registered, this
+// method registers the string and returns an ID. This method is idempotent.
 func (i *Intern) Get(s string) int {
 	if i.GetOrZero(s) == 0 {
 		i.gen++
