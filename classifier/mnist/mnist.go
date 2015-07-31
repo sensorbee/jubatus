d1649c238bf2e3c0ce79c93cb95ba5086ce6e732
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-07-31 17:33:56 +0900

    Extract one data per line file source from mnist.

--- classifier/mnist/mnist.go
+++ classifier/mnist/mnist.go
@@ -6,72 +6,50 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"pfi/sensorbee/jubatus/internal/perline"
 	"pfi/sensorbee/sensorbee/bql"
 	"pfi/sensorbee/sensorbee/core"
 	"pfi/sensorbee/sensorbee/data"
 	"strconv"
 	"strings"
-	"time"
 )
 
 type source struct {
 	path string
 }
 
-func (s *source) GenerateStream(ctx *core.Context, w core.Writer) error {
-	f, err := os.Open(s.path)
-	if err != nil {
-		return err
+func (s *source) Parse(line string, lineNo int) (data.Map, error) {
+	line = strings.TrimSpace(line)
+	fields := strings.Split(line, " ")
+	if len(fields) == 0 {
+		return nil, perline.Pass
 	}
-	defer f.Close()
-	r := bufio.NewReader(f)
-	for lineNo := 1; ; lineNo++ {
-		line, err := r.ReadString('\n')
+	label := fields[0]
+	fv := make(data.Map)
+	for _, field := range fields[1:] {
+		ix := strings.Index(field, ":")
+		if ix < 0 {
+			return nil, fmt.Errorf("invalid libsvm format at %s:%d", s.path, lineNo)
+		}
+		v, err := strconv.ParseFloat(field[ix+1:], 32)
 		if err != nil {
-			if err != io.EOF {
-				return err
-			}
-			line = strings.TrimSpace(line)
-			if len(line) == 0 {
-				return nil
-			}
+			return nil, fmt.Errorf("%v at %s:%d", err, s.path, lineNo)
 		}
-		line = strings.TrimSpace(line)
-		fields := strings.Split(line, " ")
-		if len(fields) == 0 {
-			continue
-		}
-		label := fields[0]
-		fv := make(data.Map)
-		for _, field := range fields[1:] {
-			pair := strings.Split(field, ":")
-			if len(pair) != 2 {
-				return fmt.Errorf("invalid libsvm format at %s:%d", s.path, lineNo)
-			}
-			v, err := strconv.Atoi(pair[1])
-			if err != nil {
-				return fmt.Errorf("invalid libsvm format at %s:%d: %v", s.path, lineNo, err)
-			}
-			fv[pair[0]] = data.Float(v) / 255
-		}
-		now := time.Now()
-		err = w.Write(ctx, &core.Tuple{
-			Data: data.Map{
-				"label":          data.String(label),
-				"feature_vector": fv,
-			},
-			Timestamp:     now,
-			ProcTimestamp: now,
-		})
-		if err != nil {
-			return err
-		}
+		fv[field[:ix]] = data.Float(v) / 255
 	}
-	return nil
+	return data.Map{
+		"label":          data.String(label),
+		"feature_vector": fv,
+	}, nil
 }
 
-func (s *source) Stop(*core.Context) error {
-	return nil
+func (s *source) Reader() (*bufio.Reader, io.Closer, error) {
+	f, err := os.Open(s.path)
+	if err != nil {
+		return nil, nil, err
+	}
+	r := bufio.NewReader(f)
+	return r, f, nil
 }
 
 func createSource(ctx *core.Context, ioParams *bql.IOParams, params data.Map) (core.Source, error) {
@@ -83,9 +61,9 @@ func createSource(ctx *core.Context, ioParams *bql.IOParams, params data.Map) (c
 	if err != nil {
 		return nil, fmt.Errorf("path parameter is not a string: %v", err)
 	}
-	return core.NewRewindableSource(&source{
-		path: path,
-	}), nil
+
+	s := &source{path}
+	return core.NewRewindableSource(perline.NewSource(s, s, 1)), nil
 }
 
 func init() {
---
+++ internal/perline/source.go
@@ -0,0 +1,76 @@
+package perline
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"pfi/sensorbee/sensorbee/core"
+	"pfi/sensorbee/sensorbee/data"
+	"strings"
+	"time"
+)
+
+type Source struct {
+	lp          LineParser
+	bufio       BufioReaderFactory
+	firstLineNo int
+}
+
+type LineParser interface {
+	Parse(line string, lineNo int) (data.Map, error)
+}
+type BufioReaderFactory interface {
+	Reader() (*bufio.Reader, io.Closer, error)
+}
+
+func NewSource(lp LineParser, bufio BufioReaderFactory, firstLineNo int) *Source {
+	return &Source{
+		lp:          lp,
+		bufio:       bufio,
+		firstLineNo: firstLineNo,
+	}
+}
+
+func (s *Source) GenerateStream(ctx *core.Context, w core.Writer) error {
+	r, c, err := s.bufio.Reader()
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	for lineNo := s.firstLineNo; ; lineNo++ {
+		line, err := r.ReadString('\n')
+		line = strings.TrimSpace(line)
+		if err != nil {
+			if err != io.EOF {
+				return err
+			}
+			if len(line) == 0 {
+				return nil
+			}
+		}
+		data, err := s.lp.Parse(line, lineNo)
+		if err != nil {
+			if err == Pass {
+				continue
+			}
+			return fmt.Errorf("%v at ", err)
+		}
+		now := time.Now()
+		err = w.Write(ctx, &core.Tuple{
+			Data:          data,
+			Timestamp:     now,
+			ProcTimestamp: now,
+		})
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (s *Source) Stop(*core.Context) error {
+	return nil
+}
+
+var Pass = errors.New("pass")
