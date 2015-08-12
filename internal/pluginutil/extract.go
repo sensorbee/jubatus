135431a17b66ea9b90d16f6abd9fff130439a6a6
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-08-12 21:26:08 +0900

    Add anomaly UDF plugin.

--- internal/pluginutil/extract.go
+++ internal/pluginutil/extract.go
@@ -18,6 +18,30 @@ func ExtractParamAsStringWithDefault(params data.Map, key, def string) (string, 
 	return s, nil
 }
 
+func ExtractParamAsString(params data.Map, key string) (string, error) {
+	v, ok := params[key]
+	if !ok {
+		return "", fmt.Errorf("%s parameter is missing", key)
+	}
+	s, err := data.AsString(v)
+	if err != nil {
+		return "", fmt.Errorf("%s parameter is not a string: %v", err)
+	}
+	return s, nil
+}
+
+func ExtractParamAsInt(params data.Map, key string) (int64, error) {
+	v, ok := params[key]
+	if !ok {
+		return 0, fmt.Errorf("%s parameter is missing", key)
+	}
+	x, err := data.AsInt(v)
+	if err != nil {
+		return 0, fmt.Errorf("%s parameter is not an integer: %v", key, err)
+	}
+	return x, nil
+}
+
 func ExtractParamAndConvertToFloat(params data.Map, key string) (float64, error) {
 	v, ok := params[key]
 	if !ok {
