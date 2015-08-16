fa3194fc8cf0f57668ae531883e915b56349cdf6
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-08-16 15:49:07 -0700

    Fix plugin registration of classifier

--- classifier/plugin/plugin.go
+++ classifier/plugin/plugin.go
@@ -7,28 +7,17 @@ import (
 )
 
 func init() {
-	if err := udf.RegisterGlobalUDSCreator("jubaclassifier_arow", udf.UDSCreatorFunc(classifier.NewAROWState)); err != nil {
-		panic(err)
-	}
+	udf.MustRegisterGlobalUDSCreator("jubaclassifier_arow", &classifier.AROWStateCreator{})
 
 	// The name jubaclassify is not only for AROW, but for all classifier algorithms.
 	// We have implemented only AROW, so we use the name for arowClassify for now.
 	// When we have to implement another classification algorithm, generalize jubaclassify
 	// to other algorithms. For example, define classifier.Classifier and adjust all algorithms to it.
-	if err := udf.RegisterGlobalUDF("jubaclassify", udf.MustConvertGeneric(classifier.AROWClassify)); err != nil {
-		panic(err)
-	}
+	udf.MustRegisterGlobalUDF("jubaclassify", udf.MustConvertGeneric(classifier.AROWClassify))
 
 	// TODO: consider to rename
-	if err := udf.RegisterGlobalUDF("juba_classified_label", udf.MustConvertGeneric(classifier.ClassifiedLabel)); err != nil {
-		panic(err)
-	}
+	udf.MustRegisterGlobalUDF("juba_classified_label", udf.MustConvertGeneric(classifier.ClassifiedLabel))
 
-	if err := udf.RegisterGlobalUDF("juba_classified_score", udf.MustConvertGeneric(classifier.ClassifiedScore)); err != nil {
-		panic(err)
-	}
-
-	if err := udf.RegisterGlobalUDF("juba_softmax", udf.MustConvertGeneric(math.Softmax)); err != nil {
-		panic(err)
-	}
+	udf.MustRegisterGlobalUDF("juba_classified_score", udf.MustConvertGeneric(classifier.ClassifiedScore))
+	udf.MustRegisterGlobalUDF("juba_softmax", udf.MustConvertGeneric(math.Softmax))
 }
