80100c3bb7887e2c75d8d2889b4c71921524c7dd
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-08-20 15:09:18 +0900

    Add getter to bitvector.Vector.

--- internal/math/bitvector/vector.go
+++ internal/math/bitvector/vector.go
@@ -12,6 +12,10 @@ func NewVector(bitNum int) *Vector {
 	}
 }
 
+func (v *Vector) GetAsUint64(n int) uint64 {
+	return uint64(v.data[n])
+}
+
 func (v *Vector) Set(n int) {
 	if n < 0 || n >= v.bitNum {
 		panic("TODO: fix")
