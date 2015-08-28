2aac0472fa2f7d9f68440f30d7c981b0599f7186
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-08-28 17:32:49 +0900

    Move HammingDistance implementation.

--- internal/math/bitvector/array.go
+++ internal/math/bitvector/array.go
@@ -106,7 +106,51 @@ func (a *generalArray) BitNum() int {
 }
 
 func (a *generalArray) HammingDistance(n int, v *Vector) (int, error) {
-	return HammingDistance(a, n, v)
+	if a.bitNum != v.bitNum {
+		return 0, fmt.Errorf("BitNum mismatch: %v, %v", a.bitNum, v.bitNum)
+	}
+	if n < 0 || n >= a.Len() {
+		return 0, fmt.Errorf("invalid Array index: %v", n)
+	}
+
+	lbit := n * a.bitNum
+	rbit := lbit + a.bitNum
+	l := lbit / wordBits
+	r := rbit / wordBits
+
+	nRightBits := rbit % wordBits
+
+	if lbit%wordBits == 0 {
+		x := a.data[l:]
+		ret := 0
+		for i := 0; i < r-l; i++ {
+			ret += bitcount(x[i] ^ v.data[i])
+		}
+		last := r - l
+		ret += bitcount((x[last] & leastBits(nRightBits)) ^ v.data[last])
+		return ret, nil
+	}
+
+	leftOffset := lbit % wordBits
+	nLeftBits := (wordBits - leftOffset)
+	leftBits := a.data[l] >> uint(leftOffset)
+	ret := 0
+	nfull := r - (l + 1)
+	x := a.data[l+1 : r]
+	for i := 0; i < nfull; i++ {
+		ret += bitcount(x[i] ^ v.data[i])
+	}
+	if nRightBits == 0 {
+		ret += bitcount(leftBits ^ v.data[nfull])
+	} else {
+		x := (a.data[r] & leastBits(nRightBits)) | (leftBits << uint(nRightBits))
+		ret += bitcount(x ^ v.data[nfull])
+		if nLeftBits+nRightBits > wordBits {
+			x := leftBits >> uint(wordBits-nRightBits)
+			ret += bitcount(x ^ v.data[nfull+1])
+		}
+	}
+	return ret, nil
 }
 
 func (a *generalArray) Get(n int) (*Vector, error) {
