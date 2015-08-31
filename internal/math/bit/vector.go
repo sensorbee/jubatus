d93160ac7b353db07a61a33036a6e321078e909f
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-08-31 18:34:23 +0900

    Add comments for bit.Vector.

--- internal/math/bit/vector.go
+++ internal/math/bit/vector.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 )
 
+// Vector is a bitvector.
 type Vector struct {
 	data   buf
 	bitNum int
 }
 
+// Vector creates a new bitvector. All bits are initialized with zero.
 func NewVector(bitNum int) *Vector {
 	return &Vector{
 		data:   make(buf, nWords(bitNum, 1)),
@@ -20,6 +22,7 @@ func (v *Vector) getAsUint64(n int) uint64 {
 	return uint64(v.data[n])
 }
 
+// Set sets the nth bit one.
 func (v *Vector) Set(n int) error {
 	if n < 0 || n >= v.bitNum {
 		return fmt.Errorf("invalid Vector index: %v", n)
